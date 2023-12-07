7696e6bd817d2c6650272545c238c0821f9ce0a0
YorkWil <yorkwillmann@gmail.com>
2023-12-07 23:10:06 +0100

    Update totalreflektion.c
    Rework Totalreflektion, Umfassende änderung an Program und funktion :dragon_face:

--- totalreflektion.c
+++ totalreflektion.c
@@ -17,44 +17,72 @@ Datum: 4.12.2023
 #include "totalreflektion.h"
 #define Pi (3.141592654)
 
-void totalreflektion(void) //<=totalreflektion
+void totalreflektion(void) 
 {				
-	//Variablen
+//~~~~~~~~~~~~~~~~~~~~~~~~Variablen~~~~~~~~~~~~~~~~~~~~~~~~
 	double  n1 = 0;			//Brechzahl 1. Medium
 	double  n2 = 0;			//Brechzahl 2. Medium
-	double  ergebnis = 0;  	//Winkel Theta
+	double  Tk = 0;  		//Winkel Theta
 	double  val = 0;  		//Zwischenspeicher
-	
-	while(n1 < 1)
-	{
-		//Abfrage
-		printf("\nBitte gib eine Brechzahl n1 an: \n");
-		scanf("%lf", &n1);
-		fflush(stdin);
+	double  ergebnis = 0;  	//Ergebnis
+	int a = 0; 				//Abfrage Berechnung 
+//~~~~~~~~~~~~~~~~~~~~~~~~Abfrage~~~~~~~~~~~~~~~~~~~~~~~~
+	printf("\033[0;32m\nBitte geben sie vorhandene Werte an.\nFür den zu berechnende Wert MUSS 0 angegeben werden! \033[0m\n");
+	//Brechzahl 1. Medium
+	printf("\nBitte gib eine Brechzahl n1 an: \n");
+	scanf("%lf", &n1);
+	fflush(stdin);
+	//Brechzahl 2. Medium
+	printf("\nBitte gib eine Brechzahl n2 an: \n");
+	scanf("%lf", &n2);
+	fflush(stdin);
+	//Winkel Theta
+	printf("\nBitte gib eine Winkel Tk an: \n");
+	scanf("%lf", &Tk);
+	fflush(stdin);
+//~~~~~~~~~~~~Berechnung der Formelumstellung~~~~~~~~~~~~
+	if(n1 == 0){
+		a = 2;
+		printf("Sie haben sich für die Brechzahl n1 entschieden");
 	}
-	
-	while(n2 < 1)
-	{
-		printf("\nBitte gib eine Brechzahl n2 an: \n");
-		scanf("%lf", &n2);
-		fflush(stdin);
+	else if(n2 == 0){
+		a = 3;
+		printf("Sie haben sich für die Brechzahl n2 entschieden");
+		}
+	else if(Tk == 0){
+		a = 1;
+		printf("Sie haben sich für den Winkel Tk entschieden");
+		}
+	else{
+		printf("\033[0;32m\nVERPISS DICH, DU LUMP!\033[0m\n\n");
 	}
+//~~~~~~~~~~~~~~~~~~~~~~~~Ausgabe~~~~~~~~~~~~~~~~~~~~~~~~
+switch(a){
 
-	//Berechnung
-	if (n1 > n2)
-	{
-		val = n2/n1;
-		
-		ergebnis = (asin(val))*180/Pi;
-		
-		//Ausgabe
-		printf("\nDer Winkel der Totalreflexion beträgt: %lf\n\n", ergebnis);
-	}
-	
-	else 
-	{
-		printf("\nDie erste Brechungszahl muss groesser als die zweite Brechungszahl sein!\n\n");
-	}	
+	case 1:							//Winkel Totalreflektion
+		if(n1 > n2){				//Formel ist berechebar 
+			val = n2 / n1;
+			ergebnis = (asin(val))*180/Pi;
+			printf("\nDer Winkel der Totalreflexion beträgt: %lf\n\n", ergebnis);
+		}
 
-	return;
+		else{						//Abbruchbedingung
+			printf("\nDie erste Brechungszahl muss groesser als die zweite Brechungszahl sein!\n\n");
+		}
+		break;
+
+	case 2:							//Brechzahl 1
+		val = Tk*(Pi/180);			//Berechnung Winkel
+		ergebnis = (sin(val))/n2;
+		printf("\nDie  Brechungszahl n1 beträgt: %lf\n\n", ergebnis);
+		break;
+
+	case 3:							//Brechzahl 2
+		val = Tk*(Pi/180);			//Berechnung Winkel
+		ergebnis = (sin(val))*n1;
+		printf("\nDie  Brechungszahl n2 beträgt: %lf \n\n", ergebnis);
+		break;
 }
+return;
+}
+

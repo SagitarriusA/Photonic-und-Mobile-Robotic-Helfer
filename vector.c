16f246a52b0a48d544870330b5efaedc41539fba
miimuroodo <ryoya.bauer@gmail.com>
2023-12-18 09:06:04 +0100

    update
    added comments

--- vector.c
+++ vector.c
@@ -1,12 +1,13 @@
 /* vector.c
 Beschreibung: Kreuz- und Skalarprodukt sowie Betrag eines Vektors.
 Autor: Ramon Tarca, Ryoya Bauer
-Firma: FHGR 
-Version: 1.0 
-Datum: 04.12.2023 
-Änderungen: 
-1.0 04.12.2023 Erste Version 
+Firma: FHGR
+Version: 1.0
+Datum: 18.12.2023
+Änderungen:
+1.0 04.12.2023 Erste Version
 2.0 6.12.2023 Einfuegen der while-Schleife
+3.0 18.12.2023 Kommentare hinzugefügt
 */
 
 #include <stdio.h>
@@ -24,16 +25,17 @@ float normInternal(int n, float *vector);
 void crossInternal(int n, float *vector1, float *vector2);
 float dotInternal(int n, float *vector1, float *vector2);
 
+//-----Hauprprogramm-----
 void vector()
 {
 	char ja[3] = "ja";
 	char Ja[3] = "Ja";
 	char weitereRechnungVec[3] = "ja";
-	
+
 	while(strcmp (weitereRechnungVec, ja) == 0 || strcmp (weitereRechnungVec, Ja) == 0)
 	{
 		int menue = 0;
-			
+
 		printf("\n1 - Betrag eines Vektors in n\n");
 		printf("2 - Kreuzprodukt in 3D\n");
 		printf("3 - Skalarprodukt in n\n");
@@ -41,37 +43,38 @@ void vector()
 		printf("5 - Abstand Punkt-Gerade in 3D\n");
 		scanf("%i", &menue);
 		fflush(stdin);
-		
+
 		switch(menue)
 		{
-			case 1: 
-				norm(); 
+			case 1:
+				norm();
 				break;
-			case 2: 
-				cross(); 
+			case 2:
+				cross();
 				break;
-			case 3: 
+			case 3:
 				dot();
 				break;
-			case 4: 
-				distance2D(); 
+			case 4:
+				distance2D();
 				break;
-			case 5: 
-				distance3D(); 
+			case 5:
+				distance3D();
 				break;
 			default:
 				printf("Der eingegebene Wert ist keinem Programm zugewiesen!\n\n");
 				return;
 		}
-		
+
 		printf("Willst du eine weitere Operation mit Vektoren durchfueren? Wenn ja, dann schreibe ja oder Ja.\n");
 		scanf("%s" ,&weitereRechnungVec);
 		fflush(stdin);
-		
+
 		system("cls");
 	}
 }
 
+//-----Vektorbetrag-----
 void norm(void)
 {
 	int n = 0;
@@ -79,54 +82,56 @@ void norm(void)
 	scanf("%i",&n);
 	fflush(stdin);
 	float vector1[n];
-	
+
 	printf("\nBitte den Vektor abfüllen:\n");
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%f",&vector1[i]);
 		fflush(stdin);		// Buffer leeren um scanf() nochmals auszuführen
 	}
-		
+
 	float result = 0;
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		result += vector1[i] * vector1[i];
 	}
-	
+
 	result = sqrtf(result);
-	
+
 	printf("\nDer Betrag ist %f\n\n",result);
 	return;
 }
 
+
+//-----Kreuzprodukt-----
 void cross(void)
 {
 	int n = 3;
 	float vector1[n], vector2[n];
-	
+
 	printf("\nBitte den ersten Vektor abfüllen:\n");
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%f",&vector1[i]);
 		fflush(stdin);		// Buffer leeren um scanf() nochmals auszuführen
 	}
-		
+
 	printf("\nBitte den zweiten Vektor abfüllen:\n");
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%f",&vector2[i]);
 		fflush(stdin);		// Buffer leeren um scanf() nochmals auszuführen
 	}
-	
+
 	float result[n];
-	
+
 	int count1 = 1;
 	int count2 = 2;
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		result[i] = vector1[count1] * vector2[count2] - vector1[count2] * vector2[count1];
@@ -141,7 +146,7 @@ void cross(void)
 			count2 = 0;
 		}
 	}
-	
+
 	printf("\nDer neue Vektor lautet:\n");
 	for(int i = 0; i < n; i++)
 	{
@@ -151,6 +156,7 @@ void cross(void)
 	return;
 }
 
+//-----Skalarprodukt-----
 void dot(void)
 {
 	int n = 0;
@@ -158,50 +164,52 @@ void dot(void)
 	scanf("%i",&n);
 	fflush(stdin);
 	float vector1[n], vector2[n];
-	
+
 	printf("\nBitte den ersten Vektor abfüllen:\n");
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%f",&vector1[i]);
 		fflush(stdin);		// Buffer leeren um scanf() nochmals auszuführen
 	}
-		
+
 	printf("\nBitte den zweiten Vektor abfüllen:\n");
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%f",&vector2[i]);
 		fflush(stdin);		// Buffer leeren um scanf() nochmals auszuführen
 	}
-	
+
 	float result = 0;
-	
+
 	for(int i = 0; i < n; i++)
 	{
 		result += vector1[i] * vector2[i];
 	}
-	
+
 	printf("\nDas Skalarprodukt ergibt %f\n\n",result);
 	return;
 }
 
-void tausche(float vector1[]) 
+
+//-----Punkt-Gerade Abstand in 2D mit Normalvektorfunktion (Unterprogramm nötig für div. Rechnungen)-----
+void tausche(float vector1[])
 {
     float temp = vector1[0];
     vector1[0] = -vector1[1];
     vector1[1] = temp;
 }
 
-void distance2D(void) 
+void distance2D(void)
 {
     int n = 2;
     float vector1[n], vector2[n], point[n];
     float dotrestult=0;
 
     printf("d=<P-S0, hut n>\nBitte die Gerade definieren (g):\n"); //Gerade
-	
-    for (int i = 0; i < n; i++) 
+
+    for (int i = 0; i < n; i++)
 	{
         scanf("%f", &vector1[i]);
         fflush(stdin);
@@ -260,6 +268,9 @@ void distance2D(void)
     return;
 }
 
+
+
+//-----Punkt-Gerade Abstand in 3D(Unterprogramm nötig für div. Rechnungen)-----
 void distance3D(void)
 {
 	int n = 3;
@@ -283,13 +294,13 @@ void distance3D(void)
         scanf("%f", &point[i]);
         fflush(stdin);
     }
-	
+
     for (int i = 0; i < n; i++) {
         u[i] = point[i] - vector2[i];
     }
-	
+
 	crossInternal(n, u, vector1);
-	
+
 	result = normInternal(n,u) / normInternal(n, vector1);
 
     printf("\nDer Abstand betraegt %f\n", result);
@@ -310,6 +321,9 @@ float normInternal(int n, float *vector1)
 	return result;
 }
 
+
+
+//-----Unterfunktionen (Vektorbetrag, Kreuzprodukt und Skalarprodukt)-----
 void crossInternal(int n, float *vector1, float *vector2) {
     float result[n];
     int count1 = 1;
@@ -329,7 +343,7 @@ void crossInternal(int n, float *vector1, float *vector2) {
 	for (int i = 0; i < n; i++) {
         vector1[i] = result[i];
     }
-		
+
     return;
 }
 
